6da148707d82c646ea1b206a9cfdd4b0b07e6d82
Rohit Sharma <sharmarohit469494@gmail.com>
2026-02-14 18:09:57 +0530

    my current work also done

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 console.log('Hello, Git!')
 console.log('Hello, GitHub!')
 console.log('Hello from dev-rohit')
+
 //FIXING THE BUG 
 //code
 //code
 //code
+
+//Currenty I'm implenting some feature for production
+//CODE` 
+//CODE
+//CODE 
+//CODE
+
